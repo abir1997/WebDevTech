5f873a71a3b57abef4bfe6642d8feda59ad2afe2
abir1997 <s3677701@student.rmit.edu.au>
2021-02-02 16:17:59 +1100

    Added template

---
+++ CourseManagement/CourseManagement/Data/AzureContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseManagement.Data
+{
+    public class AzureContext : DbContext
+    {
+        public AzureContext(DbContextOptions<AzureContext> options) : base(options)
+        { }
+    }
+}
