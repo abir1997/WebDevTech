fead4044e814cf060a8f8ccff32d837f262a71a6
abir1997 <s3677701@student.rmit.edu.au>
2021-02-02 16:33:16 +1100

    Added instructor model

---
+++ CourseManagement/CourseManagement/Models/Instructor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CourseManagement.Models
+{
+    public class Instructor
+    {
+        public int ID { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string FirstMidName { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Hire Date")]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime HireDate { get; set; }
+
+        public virtual IEnumerable<CourseAssignment> CourseAssignments { get; set; }
+
+        public OfficeAssignment OfficeAssignment { get; set; }
+    }
+}
