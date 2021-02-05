3ff061082d65d4409d62fe07d144b28573f35e9d
abir1997 <s3677701@student.rmit.edu.au>
2021-02-05 16:18:13 +1100

    Instructor Edit done

--- CourseManagement/CourseManagement/Controllers/InstructorController.cs
+++ CourseManagement/CourseManagement/Controllers/InstructorController.cs
@@ -1,4 +1,5 @@
 ﻿using CourseManagement.Data;
+using CourseManagement.Models;
 using CourseManagement.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -13,7 +14,7 @@ namespace CourseManagement.Controllers
     public class InstructorController : Controller
     {
         private readonly AzureContext _context;
-        
+
         public InstructorController(AzureContext context)
         {
             _context = context;
@@ -28,5 +29,54 @@ namespace CourseManagement.Controllers
                 Instructors = await instructors.ToPagedListAsync(1, 4).ConfigureAwait(false)
             });
         }
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id is null)
+            {
+                return NotFound();
+            }
+
+            var instructor = await _context.Instructors.FindAsync(id);
+
+            if (instructor is null)
+            {
+                return NotFound();
+            }
+
+            return View(instructor);
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("InstructorID, LastName, FirstMidName, HireDate")] Instructor instructor)
+        {
+            if(id != instructor.InstructorID)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(instructor);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if(!_context.Instructors.Any(x => x.InstructorID == id))
+                    {
+                        return NotFound();
+                    }
+                    throw;
+                }
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(instructor);
+        }
+
     }
 }
